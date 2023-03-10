4c1b22aed41adee87f2af74003d240dcac44788d
kevin68107 <kevin.aihoon@amalitech.com>
2023-03-10 16:18:10 +0000

    Added the functionalities

--- todo-app-main/js/index-dup.js
+++ todo-app-main/js/index-dup.js
@@ -1,61 +1,8 @@
-let count=0
-
-const taskBoxList=[];
-const taskBoxSpan=[];
-const taskBoxCancel=[];
-const taskBoxCheck=[];
-// let checkedTasks=[0,0,0,0,0,0,0,0,0];
-let checkedTasks = JSON.parse(localStorage.getItem("checkedTasksStor")) || [];
-let taskTxtList=[];
-let displayState="all";
-let isLight=true;
-const taskContainer=document.getElementById("task-container");
+const taskBoxes=document.querySelectorAll(".task");
+const newDivTxt=document.getElementById("task-input");
+// const taskContainer=document.getElementById("task-container");
 const itemsLeft=document.getElementById("itemsLeft");
 const header=document.getElementById("header");
-
-function itemsLeftFunction(){
-    let checkedTasksCount=0;
-    let itemsLeftNum=0;
-    for(let i=0;i<checkedTasks.length;i++){
-        if(checkedTasks[i]==1){
-            checkedTasksCount+=1
-        }
-    };
-
-    itemsLeftNum=(count-checkedTasksCount)
-    itemsLeft.textContent=itemsLeftNum   
-
-}
-
-function renderTaskList () {
-
-    if(localStorage.getItem("countStor")){
-        count=parseInt(localStorage.getItem("countStor"));
-        taskTxtList=JSON.parse(localStorage.getItem("taskTxtListStor"));
-        checkedTasks = JSON.parse(localStorage.getItem("checkedTasksStor"));
-        isLight=("true" ==localStorage.getItem("isLightStor"));
-        for(let i=0; i<count; i++){
-            // displayTasks(i)
-            taskContainer.innerHTML += renderTaskItem(taskTxtList[i]);
-            
-        };
-        addCancelEvent();
-        addCheckToTasks();
-        itemsLeftFunction();
-        
-        if(!isLight){
-            darkToLightMode()
-        }
-    
-    }
-};
-    
-    
-
-
-const newDivTxt=document.getElementById("task-input");
-
-
 const clearBtn=document.getElementById("clearBtn");
 const displayAll=document.getElementById("displayAll");
 const displayActive=document.getElementById("displayActive");
@@ -64,35 +11,77 @@ const infoDiv=document.getElementById("contentFooter");
 const displayStates=document.getElementById("displayStates");
 const addNewTask=document.getElementById("addNewTask");
 const darkLightBtn=document.getElementById("mode-svg");
+const draggables = document.querySelectorAll('.task');
+const containers = document.querySelectorAll('.main-content');
+
+let count=0
+const taskBoxList=[];
+const taskBoxSpan=[];
+const taskBoxCancel=[];
+const taskBoxCheck=[];
+let checkedTasks=[0,0,0,0,0,0,0,0,0];
+// let checkedTasks = JSON.parse(localStorage.getItem("checkedTasksStor")) || [];
+let taskTxtList=[];
+let displayState="all";
+let isLight=true;
 
 
-renderTaskList()
+// function renderTaskList () {
+    
+    // };
+    
+    
+    
+    
+    
+    
+    taskBoxes.forEach(el=>taskBoxList.push(el));
+    taskBoxes.forEach(el=>taskBoxSpan.push(el.children.item(1)));
+    taskBoxes.forEach(el=>taskBoxCancel.push(el.children.item(2).children.item(0)));
+    taskBoxes.forEach(el=>taskBoxCheck.push(el.children.item(0)));
+    
+    
+    if(localStorage.getItem("countStor")){
+        count=parseInt(localStorage.getItem("countStor"));
+        taskTxtList=JSON.parse(localStorage.getItem("taskTxtListStor"));
+        checkedTasks = JSON.parse(localStorage.getItem("checkedTasksStor"));
+        isLight=("true" ==localStorage.getItem("isLightStor"));
+        for(let i=0; i<count; i++){
+            displayTasks(i)
+            // taskContainer.innerHTML += renderTaskItem(taskTxtList[i]);
+            
+        };
+        // addCancelEvent();
+        addCheckToTasks();
+        itemsLeftFunction();
+        
+        if(!isLight){
+            darkToLightMode()
+        }
+        
+    };
+    // renderTaskList()
+    
+    
+    
+//     function renderTaskItem(task) {
+//         // console.log(task);
+//     return `
+//     <div class="task border-radius" style="display: flex;">
+//     <div class="check-box">
+//     <div class="check">
+//     <img src="./images/icon-check.svg" alt="check">
+//     </div>
+//     </div>
+//     <span>${task}</span>
+//     <div class="cancel">
+//     <img src="./images/icon-cross.svg" alt="delete">
+//     </div>
+//     </div>
+//     `;
+// }
 
-const taskBoxes=document.querySelectorAll(".task");
 
-         
-function renderTaskItem(task) {
-    // console.log(task);
-    return `
-    <div class="task border-radius" style="display: flex;">
-    <div class="check-box">
-    <div class="check">
-    <img src="./images/icon-check.svg" alt="check">
-    </div>
-    </div>
-    <span>${task}</span>
-    <div class="cancel">
-    <img src="./images/icon-cross.svg" alt="delete">
-    </div>
-  </div>
-  `;
-}
-
-
-taskBoxes.forEach(el=>taskBoxList.push(el));
-taskBoxes.forEach(el=>taskBoxSpan.push(el.children.item(1)));
-taskBoxes.forEach(el=>taskBoxCancel.push(el.children.item(2).children.item(0)));
-taskBoxes.forEach(el=>taskBoxCheck.push(el.children.item(0)));
 // console.log(taskBoxes);
 
 
@@ -106,31 +95,41 @@ darkLightBtn.addEventListener("click",function(){
 
 newDivTxt.addEventListener("keypress", function(e){
     if(e.key==="Enter"){
-        const value = newDivTxt.value.trim();
-         if(newDivTxt.value.trim()==""){
+        // const value = newDivTxt.value.trim();
+        if( count=== 10){
+            alert("Reached max amount of tasks")
+        }
+         else if(newDivTxt.value.trim()==""){
             alert("Can't add an empty task")
         }
         else{
             taskTxtList.push(newDivTxt.value.trim());
             localStorage.setItem("taskTxtListStor",JSON.stringify(taskTxtList));
             newDivTxt.value="",
-            // displayTasks(count);
-            console.log(checkedTasks);
+            displayTasks(count);
+            // console.log(checkedTasks);
             count+=1;
-            checkedTasks.push(0)
+            // checkedTasks.push(0)
             localStorage.setItem("countStor",count);
             localStorage.setItem("checkedTasksStor",JSON.stringify(checkedTasks))
-            taskContainer.innerHTML += renderTaskItem(value);
+            // taskContainer.innerHTML += renderTaskItem(value);
         };
         itemsLeftFunction();
     }
     
 });
 
+taskBoxCancel.forEach(cancel=>cancel.addEventListener("click",function(){
+    let indexCancel=taskBoxCancel.indexOf(cancel);
+    let taskToDelete=taskBoxSpan[indexCancel].textContent;
+    let indexTaskToDelete=taskTxtList.indexOf(taskToDelete);
+    deleteTask(indexTaskToDelete,indexCancel);
+}));
+
 // console.log(taskBoxCancel);
 
-function addTaskBoxCheck() {
-
+// function addTaskBoxCheck() {
+    
     
     taskBoxCheck.forEach(check=>check.addEventListener("click",function(){
         let indexCheck=taskBoxCheck.indexOf(check);
@@ -154,8 +153,8 @@ function addTaskBoxCheck() {
         
         itemsLeftFunction()
     }));
-}
-
+    // }
+    
 clearBtn.addEventListener("click",function(){
     while(checkedTasks.includes(1)){
         for(let i=0;i<checkedTasks.length;i++){
@@ -175,7 +174,7 @@ displayAll.addEventListener("click",function(){
         }
     }
     for(let i=0;i<count;i++){
-        // displayTasks(i);
+        displayTasks(i);
         displayState="all";
     };
 });
@@ -191,7 +190,7 @@ displayActive.addEventListener("click", function(){
     let firstDisplayedTask=true;
     for(let i=0;i<count;i++){
         if(checkedTasks[i]==0){
-            // displayTasks(i);
+            displayTasks(i);
             if(firstDisplayedTask){
                 taskBoxList[i].classList.add("border-radius");
                 firstDisplayedTask=!firstDisplayedTask;
@@ -212,7 +211,7 @@ displayCompleted.addEventListener("click",function(){
     let firstDisplayedTask=true;
     for(let i=0;i<count;i++){
         if(checkedTasks[i]==1){
-            // displayTasks(i);
+            displayTasks(i);
             if(firstDisplayedTask){
                 taskBoxList[i].classList.add("border-radius")
                 firstDisplayedTask=!firstDisplayedTask;
@@ -224,24 +223,24 @@ displayCompleted.addEventListener("click",function(){
 
 
 
-// function displayTasks(num){
-//     taskBoxSpan[num].textContent=taskTxtList[num];
-//     taskBoxList[num].style.display="flex";
-// };
+function displayTasks(num){
+    taskBoxSpan[num].textContent=taskTxtList[num];
+    taskBoxList[num].style.display="flex";
+};
 
 function deleteTask(index1, index2){
-    console.log(index1, index2);
+    // console.log(index1, index2);
     taskTxtList.splice(index1,1);
     count-=1;
     localStorage.setItem("taskTxtListStor",JSON.stringify(taskTxtList));
     localStorage.setItem("countStor",count);
-    taskBoxList[index1].remove()
-    // taskBoxList.forEach(el=>el.style.display="none");    
-
+    // taskBoxList[index1].remove()
+    taskBoxList.forEach(el=>el.style.display="none");    
+    
     checkedTasks.splice(index2,1);
     checkedTasks.push(0);
     localStorage.setItem("checkedTasksStor",JSON.stringify(checkedTasks));
-
+    
     taskBoxCheck.forEach(el=>el.children.item(0).classList.remove("inner-check-style"));
     taskBoxCheck.forEach(el=>el.classList.remove("outer-check-style"));
     taskBoxCheck.forEach(el=>el.children.item(0).children.item(0).classList.remove("inner-check-img"));
@@ -249,27 +248,27 @@ function deleteTask(index1, index2){
     taskBoxSpan.forEach(el=>el.classList.remove("checked-span-LM"));
     
     addCheckToTasks();
-
+    
     if(displayState=="all"){
         for(let i=0;i<count;i++){
-            // displayTasks(i)
+            displayTasks(i)
         };
     }
     else if(displayState=="active"){
         for(let i=0;i<count;i++){
             if(checkedTasks[i]==0){
-                // displayTasks(i);
+                displayTasks(i);
             };
         };
     }
     else{
         for(let i=0;i<count;i++){
             if(checkedTasks[i]==1){
-                // displayTasks(i);
+                displayTasks(i);
             };
         };
     }
-
+    
     // renderTaskList();
     itemsLeftFunction();
 };
@@ -294,24 +293,52 @@ function addCheckToTasks(){
 };
 
 
-function addCancelEvent() {
-
-    taskBoxCancel.forEach(cancel=>cancel.addEventListener("click",function(){
-        let indexCancel=taskBoxCancel.indexOf(cancel);
-        let taskToDelete=taskBoxSpan[indexCancel].textContent;
-        let indexTaskToDelete=taskTxtList.indexOf(taskToDelete);
-        deleteTask(indexTaskToDelete,indexCancel);
-    }));
-}
+// function addCancelEvent() {
     
-addCancelEvent();
-
-function displayStateColor(displayX){
+    // }
+    
+    // addCancelEvent();
+    function itemsLeftFunction(){
+        let checkedTasksCount=0;
+        let itemsLeftNum=0;
+        for(let i=0;i<checkedTasks.length;i++){
+            if(checkedTasks[i]==1){
+                checkedTasksCount+=1
+            }
+        };
+    
+        itemsLeftNum=(count-checkedTasksCount)
+        itemsLeft.textContent=itemsLeftNum   
+    
+    }
+    
+    function displayStateColor(displayX){
     displayActive.classList.remove("display-states-selected");
     displayAll.classList.remove("display-states-selected");
     displayCompleted.classList.remove("display-states-selected");
     displayX.classList.add("display-states-selected");
 }
+
+
+// Draging function
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () =>{
+        draggable.classList.add('dragging')
+    })
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+})
+})
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault()
+       const draggable =  document.querySelector('.dragging')
+       container.appendChild(draggable)
+})
+})
+
+
 
 function darkToLightMode(){
     header.classList.toggle("header-darkmode");
@@ -323,7 +350,7 @@ function darkToLightMode(){
     taskBoxList.forEach(e=>e.classList.toggle("task-border-darkMode"));
     taskBoxList.forEach(e=>e.classList.toggle("task-shadow-darkMode"));
     infoDiv.classList.toggle("display-states-darkMode");
-    infoDiv.classList.toggle("info-div-shadow-LM");
+    infoDiv.classList.toggle("content-footer-darkMode");
     displayStates.classList.toggle("display-states-darkMode");
     displayStates.classList.toggle("display-states-shadow-darkMode");
     displayActive.classList.toggle("display-states-span-darkMode");
